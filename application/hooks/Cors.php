5e494b7a640a24101c12130553bc3a8a70a31f96
vijay webmart <vijays.webmart@gmail.com>
2025-07-20 12:09:47 +0530

    update cors police issue

--- application/hooks/Cors.php
+++ application/hooks/Cors.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Cors {
     public function enable() {
-        header("Access-Control-Allow-Origin: http://localhost:8080"); // restrict to your frontend
+        header("Access-Control-Allow-Origin: *"); // restrict to your frontend
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
         
